8098a5bc990f55e6ceef2a9be944dd24d208f63e
Alveiro H <alveiro7@gmail.com>
2021-08-21 21:38:49 -0500

    [add] HEAD detached from

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import gravatar from '../utils/gravatar'
 import '../scss/Header.scss'
 import logo from '../assets/images/logo-platzi-video-BW2.png'
 import userIcon from '../assets/icons/user-icon.png'
-import { Link } from 'react-router-dom'
 
-const Header = () => (
-    <header className='header'>
-            <Link to="/">
-                <img className='header__img' src={logo} alt='logo platzi video'/>
-            </Link>
-        <div>
-            <figure className="header__profile">
-                <img src={userIcon} alt="icono del perfil" />
-                <figcaption>Perfil</figcaption>
-            </figure>
-            <nav className= 'header__menu'>
-                <ul>
-                    <li><Link to="/login">cuenta</Link></li>
-                    <li><a href="">cerrar sesión</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-)
 
-export default Header
+const Header = props => {
+    const {user} = props
+    const handUser = Object.keys(user).length > 0
+    return(
+        <header className='header'>
+                <Link to="/">
+                    <img className='header__img' src={logo} alt='logo platzi video'/>
+                </Link>
+            <div>
+                <figure className="header__profile">
+                    {handUser ?
+                        <img src={gravatar(user.email)} alt={user.email} />
+                        :
+                        <img src={userIcon} alt="icono del perfil" />
+                    }
+                    <figcaption>Perfil</figcaption>
+                </figure>
+                <nav className= 'header__menu'>
+                    <ul>
+                        <li><Link to="/login">cuenta</Link></li>
+                        <li><a href="">cerrar sesión</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect (mapStateToProps, null)(Header)
